086683bc0e58150defda29fecc10958022f04227
hienjoy <wisdom16no@naver.com>
2023-11-13 03:40:19 +0900

    5주차 실습

---
+++ 5week/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import TV from "./pages/TV";
+import Celebrity from "./pages/Celebrity";
+import NotFound from "./pages/NotFound";
+import Header from "./Component/Header";
+
+function App() {
+  return (
+    <div className="root-wrap">
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie" element={<Movies />} />
+            <Route path="/tv" element={<TV />} />
+            <Route path="/person" element={<Celebrity />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
